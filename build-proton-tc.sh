5966e30a3788d52674cb24106e2d3766674a08af
Danny Lin <danny@kdrag0n.dev>
2019-07-23 18:29:52 -0700

    build-proton-tc.sh: Introduce specialized Proton toolchain build script
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ build-proton-tc.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Script to build a toolchain specialized for Proton Kernel development
+
+# Exit on error
+set -e
+
+# Function to show an informational message
+function msg() {
+    echo -e "\e[1;32m$@\e[0m"
+}
+
+# Build LLVM
+msg "Building LLVM..."
+./build-llvm.py \
+	--clang-vendor "Proton" \
+	--projects "clang;compiler-rt;lld;polly" \
+	--targets "AArch64;ARM" \
+	--march "skylake" \
+	--pgo
+
+# Build binutils
+msg "Building binutils..."
+./build-binutils.py \
+	--targets arm aarch64
+
+# Strip products
+msg "Stripping products..."
+for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
+	strip ${f: : -1}
+done
