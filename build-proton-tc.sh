40b9536074371b3a0f4fff481c8c57b1ac9715d3
Danny Lin <danny@kdrag0n.dev>
2019-07-27 18:22:29 -0700

    build-proton-tc.sh: Segregate full and CI build configurations
    We need to use reduced settings when building on CI due to time
    restrictions, but we can afford to build a maxed-out toolchain with as
    much optimization as possible and support for x86 targets when we're
    building locally. Add separate build configurations based on Drone CI's
    environment variables and/or arguments to allow this.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- build-proton-tc.sh
+++ build-proton-tc.sh
@@ -9,19 +9,29 @@ function msg() {
     echo -e "\e[1;32m$@\e[0m"
 }
 
+# Configure LLVM build based on environment or arguments
+if [[ "$1" == "--ci" ]] || [[ "$DRONE" == "true" ]]; then
+	msg "Configuring reduced LLVM build for CI..."
+	llvm_args=(--targets "ARM;AArch64" --shallow-clone)
+	binutils_args=(--targets arm aarch64 --shallow-clone)
+else
+	msg "Configuring full-fledged LLVM build..."
+	llvm_args=(--targets "ARM;AArch64;X86" --march "native")
+	binutils_args=(--targets arm aarch64 x86_64)
+fi
+
 # Build LLVM
 msg "Building LLVM..."
 ./build-llvm.py \
 	--clang-vendor "Proton" \
 	--projects "clang;compiler-rt;lld;polly" \
-	--targets "AArch64;ARM" \
-	--march "skylake" \
-	--pgo
+	--pgo \
+	"${llvm_args[@]}"
 
 # Build binutils
 msg "Building binutils..."
 ./build-binutils.py \
-	--targets arm aarch64
+	"${binutils_args[@]}"
 
 # Remove unused products
 msg "Removing unused products..."
