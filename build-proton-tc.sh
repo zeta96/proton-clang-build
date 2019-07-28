8dd42d22199d1ef6aecd1c6483eea62579f8ca1c
Danny Lin <danny@kdrag0n.dev>
2019-07-27 18:21:04 -0700

    build-proton-tc.sh: Remove unused products from final toolchain
    We'll never use the LLVM headers or static libraries. Remove them to
    save approximately 155 MiB of space in the final Zstd-compressed
    toolchain when built with PGO + full LTO.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- build-proton-tc.sh
+++ build-proton-tc.sh
@@ -23,8 +23,13 @@ msg "Building binutils..."
 ./build-binutils.py \
 	--targets arm aarch64
 
-# Strip products
-msg "Stripping products..."
+# Remove unused products
+msg "Removing unused products..."
+rm -fr install/include
+rm -f install/lib/*.a install/lib/*.la
+
+# Strip remaining products
+msg "Stripping remaining products..."
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
 	strip ${f: : -1}
 done
