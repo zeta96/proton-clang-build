bf855afa4d98b14c84c4886f252cec214910260f
Danny Lin <danny@kdrag0n.dev>
2019-10-02 14:33:21 -0700

    build-proton-tc.sh: Optimize binutils for the native CPU architecture
    Now that build-binutils.sh no longer defaults to this, specify it
    explicitly for full-fledged builds.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- build-proton-tc.sh
+++ build-proton-tc.sh
@@ -17,7 +17,7 @@ if [[ "$1" == "--ci" ]] || [[ "$DRONE" == "true" ]]; then
 else
 	msg "Configuring full-fledged LLVM build..."
 	llvm_args=(--targets "ARM;AArch64;X86" --march "native" --lto full)
-	binutils_args=(--targets arm aarch64 x86_64)
+	binutils_args=(--targets arm aarch64 x86_64 --march "native")
 fi
 
 # Build LLVM
